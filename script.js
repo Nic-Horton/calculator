2aad64fee927e3ff71b613f481b4d54ddf1ffe15
Nic-Horton <77477203+Nic-Horton@users.noreply.github.com>
2023-02-13 14:38:25 -0600

    Create basic calculator

--- script.js
+++ script.js
@@ -1,41 +1,115 @@
+let displayValue = '';
+let operatorValue = '';
+let prevNumber = '';
+let currentNumber = '';
+let newOperator = '';
+let decCount = 0;
+
 function add(a,b) {
     const result = a + b;
-    return result;
+    return result.toFixed(2);
 }
 
 function subtract(a,b) {
     const result = a - b;
-    return result;
+    return result.toFixed(2);
 }
 
 function multiply(a,b) {
     const result = a * b;
-    return result;
+    return result.toFixed(2);
 }
 
 function divide(a,b) {
     const result = a / b;
-    return result;
+    return result.toFixed(2);
 }
 
 function operate(operator,a,b) {
+    if (newOperator === prevNumber){
+        return clear();
+    }
+    a = parseFloat(a);
+    b = parseFloat(b);
     switch(operator) {
         case '+':
             return add(a,b);
         case '-':
             return subtract(a,b);
-        case '*':
+        case 'x':
             return multiply(a,b);
-        case '/':
+        case '÷':
             return divide(a,b);
     }
 }
 
 const screen = document.querySelector('.screen');
-const prevScreen = document.createElement('div');
-prevScreen.className = 'previousScreen';
+const previousScreen = document.createElement('div');
+previousScreen.className = 'previousScreen';
+previousScreen.textContent = '';
 const currentScreen = document.createElement('div');
 currentScreen.className = 'currentScreen';
+currentScreen.textContent = '';
 
-screen.appendChild(prevScreen);
+screen.appendChild(previousScreen);
 screen.appendChild(currentScreen);
+
+const numberButtons = document.querySelectorAll('.numBtn');
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (button.id === 'dec'){
+            decCount++;
+        }
+        if (button.id === 'dec' && decCount > 1){
+            return;
+        }
+        displayValue += button.textContent;
+        currentNumber = displayValue;
+        currentScreen.textContent = displayValue;
+    });
+});
+
+const opButtons = document.querySelectorAll('.opBtn');
+opButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (currentNumber == ''){
+            return;
+        } else {
+        decCount = 0;
+        operatorValue = button.textContent;
+        prevNumber = currentNumber;
+        currentNumber = '';
+        displayValue = '';
+        previousScreen.textContent = (prevNumber + ' ' + operatorValue);
+        }
+    });
+});
+
+const equalButton = document.querySelector('.equal');
+equalButton.addEventListener('click', () => {
+    decCount = 0;
+    if (prevNumber == '' || currentNumber == ''){
+        return;
+    } else {
+    previousScreen.textContent = (prevNumber + ' ' + operatorValue + ' ' + currentNumber + ' =');
+    currentNumber = operate(operatorValue,prevNumber,currentNumber);
+    currentScreen.textContent = currentNumber;
+    displayValue = '';
+    newOperator = prevNumber;
+    }
+});
+
+function clear() {
+    displayValue = '';
+    operatorValue = '';
+    prevNumber = '';
+    currentNumber = '';
+    currentScreen.textContent = '';
+    previousScreen.textContent = '';
+    decCount = 0;
+}
+
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', () => {
+    clear();
+});
